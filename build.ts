c87b9a5abfa0eb2db196114f55718c39721bea2e
Chris <chrisbreuer93@gmail.com>
2025-10-03 21:53:05 -0700

    chore: wip

--- build.ts
+++ build.ts
@@ -1,8 +1,9 @@
 import { dts } from 'bun-plugin-dtsx'
 
 await Bun.build({
-  entrypoints: ['src/index.ts'],
+  entrypoints: ['src/index.ts', 'src/cli.ts'],
   outdir: './dist',
+  splitting: true,
   target: 'bun',
   plugins: [dts()],
 })
